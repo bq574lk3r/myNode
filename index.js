70dcb071e0bce8867a58af1b0eb513013ec601fd
bq574lk3r <bq.574lk3r@gmail.com>
2023-11-29 22:50:16 +0300

    added post and get methods

--- index.js
+++ index.js
@@ -10,9 +10,79 @@ app.get('/api/hello', (req, res) => {
 app.use(express.json());
 
 app.post('/api/echo', (req, res) => {
-    
     res.send(req.body.message)
+})
 
+const users = [
+    {
+        id:0,
+        username: 'testUser',
+        email: 'test@MediaList.com',
+        _password: 'testPass',
+    }
+];
+
+class User {
+    static count = 1;
+    constructor(username, email, password) {
+        try {
+            this.id = User.count;
+            this.username = username;
+            this.email = email;
+            this._password = password;
+            User.count++;
+        } catch (error) {
+            return error
+        }
+
+
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    set password(value) {
+        this._password = value;
+    }
+}
+
+app.post('/api/users', (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!!users.find(el => el.username === username || el.email === email)) {
+        res.status(400)
+        res.send('пользватель с таким username или почтовым ящиком зарегестрирован')
+        return;
+    }
+    
+    const currentUser = new User(username, email, password)
+    users.push(currentUser)
+    res.status(201)
+    res.send(currentUser)
+})
+
+app.get('/api/users', (req, res) => {
+    res.send(users.reduce((result, item) => {
+        const { id, username, email } = item
+        result.push({
+            id: id,
+            username: username,
+            email: email,
+        })
+        return result;
+    }, []))
+
+})
+
+app.get('/api/users/:id', (req, res) => {
+    const { id, username, email } = users.find(el => el.id == req.params.id)
+    const userById = {
+        id: id,
+        username: username,
+        email: email,
+    }
+    res.send(userById);
 })
 
 app.listen(PORT, () => {
