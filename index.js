445f6947fd39d9f27f0c934620d2dac89975584e
bq574lk3r <bq.574lk3r@gmail.com>
2023-12-05 01:40:51 +0300

    install and use env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+require('dotenv').config();
+const PORT = process.env.PORT;
 
 //Маршруты (Роуты)
 app.get('/api/hello', (req, res) => {
